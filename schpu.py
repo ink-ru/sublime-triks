deb6a25b8fad82017d948a7603fdc76f36ddca54
Васин Юрий <george.a.wise@gmail.com>
2016-07-24 19:43:37 +0300

    now line by line coversion

--- schpu.py
+++ schpu.py
@@ -1,4 +1,3 @@
-# TODO: make single function "transform"
 # TODO: add multyselection support
 import sublime, sublime_plugin, re
 
@@ -6,41 +5,82 @@ class schpuCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 
 		logMsg = ""
+		found = 0
+		new_content = ""
 
-		def transform(s,mode):
-			s = re.sub(r"\n{2,}", "\n", s)
-			s = re.sub(r"\s+\Z", "", s)
-			# if(mode is 'list'):
-			# 	s = re.sub(r"^(\S+)\n(\S+)(\n|\Z)", r"\1 \2\n", s)
-			s = re.sub(r"(\S+)\n(\S+)(\n|\Z)", r"\1 \2\n", s)
-			s = re.sub(r"\S+/([^/]+)/?([ ]+[^\n]+)", r"'\1' => \2", s)
-			s = re.sub(r"(\S+ =>).* \S+/([^/]+)/?(\n|\Z)", r"\1 '\2',\3", s)
-			return s
+		def transform(s):
+			flag = 0
+			new_line = ''
+
+			first = re.search("^(\S+)\s", s)
+			if first is not None:
+				first = first.group(1)
+				first = re.search("/([^/]+)/?$", s)
+				if first is not None:
+					first = first.group(1)
+					new_line += "'" + first + "' => '"
+					flag = flag + 1
+
+			second = re.search("\s(\S+)$", s)
+			if second is not None:
+				second = second.group(1)
+				second = re.search("/([^/]+)/?$", s)
+				if second is not None:
+					second = second.group(1)
+					new_line += second + "',"
+					flag = flag + 1
+
+			if flag  == 2:
+				return new_line
+			else:
+				return False
 
 		if self.view.size():
 			if len(self.view.sel()[0]) > 0:
 				selection = self.view.substr(self.view.sel()[0]).replace("'", "").replace("\"", "").replace("\t", " ")
-				sm = re.search('(\S+)[ ]+[^\s]*[ ]*(\S+)(\n|\Z)', selection)
 
-				if sm is not None:
-					selection = transform(selection,'normal')
-				else:
-					selection = transform(selection,'list')
-				self.view.replace(edit, self.view.sel()[0], selection)
+				content_list = selection.strip().splitlines()
+				if clen(content_list) > 0:
+					for item in content_list:
+						line = item.strip()
+						# if line == "" or ((' ' in line) == False and ('\t' in line) == False):
+						if line == "" or line.find(' ') <= 0:
+							continue
+						try_conv = transform(line)
+						if try_conv != False:
+							new_content = new_content + try_conv + "\n"
+							found = found + 1
+						else:
+							new_content = new_content + line + "\n"
+
+				self.view.replace(edit, self.view.sel()[0], new_content)
 
 			else:
 				dregion = sublime.Region(0, self.view.size())
 				content = self.view.substr(dregion).replace("'", "").replace("\"", "").replace("\t", " ")
 
-				m = re.search('(\S+)[ ]+[^\s]*[ ]*(\S+)(\n|\Z)', content)
-				if m is not None:
-					content = transform(content,'normal')
-				else:
-					content = transform(content,'list')
-				self.view.replace(edit, dregion, content)
+				content_list = content.strip().splitlines()
+				if len(content_list) > 0:
+					for item in content_list:
+						line = item.strip()
+						# if line == "" or ((' ' in line) == False and ('\t' in line) == False):
+						if line == "" or line.find(' ') <= 0:
+							continue
+						try_conv = transform(line)
+						if try_conv != False:
+							new_content = new_content + try_conv + "\n"
+							found = found + 1
+						else:
+							new_content = new_content + line + "\n"
+
+				self.view.replace(edit, dregion, new_content)
 			
-			logMsg += "Done!"
+			if found > 0:
+				logMsg += "Done! Found " + str(found) + " from " + str(len(content_list)) + " lines."
+			else:
+				logMsg += "Nothing found."
 		else:
 			logMsg += "Empty document!"
 
-		sublime.status_message(sublime.platform()+' '+logMsg)
+		sublime.status_message(logMsg)
+		sublime.message_dialog(logMsg)
