89b5c7635b7ca1eb399b35fe08989dad64d16dda
Васин Юрий <george.a.wise@gmail.com>
2016-08-11 11:24:54 +0300

    Update mark_changes.py

--- mark_changes.py
+++ mark_changes.py
@@ -1,21 +1,16 @@
 import sublime, sublime_plugin, types
 
+class ClearMarkedRegionsCommand(sublime_plugin.EventListener):
+	def on_post_save(self, view):
+		view.erase_regions('unsaved_changes')
 
-this_settings = 'kpi.sublime-settings'
-settings = sublime.load_settings(this_settings)
+class HighlightUnsavedCommand(sublime_plugin.EventListener):
+	def on_modified(self, view):
+		self.settings = sublime.load_settings('kpi.sublime-settings')
+		self.activity_flag = self.settings.get("mark_changes")
+		self.activity_flag = self.activity_flag if (self.activity_flag is not None) and (len(self.activity_flag) > 0) else 0
 
-activity_flag = settings.get("mark_changes")
-activity_flag = activity_flag if (activity_flag is not None) and (len(activity_flag) > 0) else 0
-
-if activity_flag:
-
-	class ClearMarkedRegionsCommand(sublime_plugin.EventListener):
-		def on_post_save(self, view):
-			view.erase_regions('unsaved_changes')
-
-	class HighlightUnsavedCommand(sublime_plugin.EventListener):
-		def on_modified(self, view):
-			
+		if int(self.activity_flag) > 0:
 			unsaved_changes = view.get_regions('unsaved_changes') + [view.line(s) for s in view.sel()]
 
 			if not isinstance(view.file_name(), type(None)):	
