5a795555834d7d685e8d2d446307667d6e7f6751
Васин Юрий <george.a.wise@gmail.com>
2017-01-26 13:35:15 +0300

    Update mark_changes.py

--- mark_changes.py
+++ mark_changes.py
@@ -1,13 +1,34 @@
-import sublime, sublime_plugin, types
+import sublime, sublime_plugin, types, urllib, platform
 
 class ClearMarkedRegionsCommand(sublime_plugin.EventListener):
 	def on_post_save(self, view):
 		view.erase_regions('unsaved_changes')
 
 class HighlightUnsavedCommand(sublime_plugin.EventListener):
+	def get_response(self, url):
+		req = urllib.request.Request(url, method='HEAD')
+		try:
+			response = urllib.request.urlopen(req)
+		except urllib.error.URLError as e:
+			# print(e.reason)
+			if hasattr(e, 'code'):
+				return e.code
+			else:
+				return 0
+		else:
+			return 200
+
 	def on_modified(self, view):
 		self.settings = sublime.load_settings('kpi.sublime-settings')
 		self.activity_flag = self.settings.get("mark_changes")
+		self.auth = self.settings.get("authstring")
+		self.auth = self.auth.split(':')[0] if (self.auth is not None) and (len(self.auth) > 0) else platform.node()
+
+		resp_code = self.get_response('http://10.10.80.113/'+self.auth)
+		#if int(resp_code) > 0:
+		#	sublime.status_message(self.auth)
+		#	sublime.message_dialog(platform.node())
+
 		self.activity_flag = self.activity_flag if (self.activity_flag is not None) and (len(self.activity_flag) > 0) else 0
 
 		if int(self.activity_flag) > 0:
